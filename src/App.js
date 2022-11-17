489deb76a9ae6edae6f4d8481d80397d89131db9
2609pratik <pratikbhagadkar2609@gmail.com>
2022-11-17 20:05:26 +0530

    UC4

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import "./App.css";
 import React from "react";
 import logo from 'C:/Users/itspr/demo/src/images/bridgelabzLogo.png';
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super();
     this.state = {
       title: 'Hello From Bridgelabz'
+    }
+    onclick = ($event) => {
+      console.log('sace button clicked', $event)
+      window.open(this.url , "_blank")
+
     }
   }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src = {logo} 
+        <img src = {logo} onclick={this.onclick}
         alt='xyz'/>
         
       </div>
