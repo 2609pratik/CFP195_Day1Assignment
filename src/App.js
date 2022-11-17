1ff8d98a058d085121052dbca109668780599cd0
2609pratik <pratikbhagadkar2609@gmail.com>
2022-11-18 00:35:29 +0530

    UC5

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import "./App.css";
 import React from "react";
-import logo from 'C:/Users/itspr/demo/src/images/bridgelabzLogo.png';
+import logo from "C:/Users/itspr/demo/src/images/bridgelabzLogo.png";
 class App extends React.Component {
-  url = 'https://www.bridgelabz.com/'
-  constructor(){
+  url = "https://www.bridgelabz.com/";
+  constructor() {
     super();
     this.state = {
-      title: 'Hello From Bridgelabz'
-    }
-    onclick = ($event) => {
-      console.log('sace button clicked', $event)
-      window.open(this.url , "_blank")
-
-    }
+      name: "",
+    };
   }
+    onClick = ($event) => {
+      console.log("save button clicked", $event);
+      window.open(this.url, "_blank");
+    };
+    onNameChange = (event) => {
+      console.log("value is ", event.target.value);
+      this.setState({ name: event.target.value });
+    };
+  
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src = {logo} onclick={this.onclick}
-        alt='xyz'/>
-        
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.name} from bridgelabz</h1>
+          <img src={logo} onClick={this.onClick}
+           alt="xyz" />
+        </div>
+        <div>
+          <input onChange={this.onNameChange}/>
+        </div>
+      </>
     );
   }
 }
